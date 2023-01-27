d7abb1c7e69d2cc49d082771af55ae8b5c483ecd
Painkillerap <painkiller.ap@gmail.com>
2023-01-27 13:51:54 +0300

    KontrV2

--- DZ/Program.cs
+++ DZ/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+string[] Array = { "Hello", "String", "2", "World", ";)", "12", "Rim" };
+string[] tempArray = new string[Array.Length];
+ArrayMin3(Array, tempArray);
+PrintArray(tempArray);
+
+string[] ArrayMin3(string[] Array, string[] tempArray)
+{
+    int count = 1;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i].Length <= 3)
+        {
+            tempArray[count] = Array[i];
+            count++;
+        }
+    }
+    return tempArray;
+}
+
+void PrintArray(string[] Array)
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+    System.Console.Write($"{Array[i]} ");
+    }
+}
